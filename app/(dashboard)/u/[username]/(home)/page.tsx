e61fa324a578733ee61c1be8f7200162a4317e99
Italoomoura <italomourasant9@gmail.com>
2024-06-12 06:55:58 -0300

    livekit webhooks

--- app/(dashboard)/u/[username]/(home)/page.tsx
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,7 +1,28 @@
-const CreatorPage = () => {
+import { getUserByUsername } from "@/lib/user-service";
+import { currentUser } from "@clerk/nextjs/server";
+
+interface CreatorPageProps {
+    params: {
+        username: string;
+    };
+};
+
+const CreatorPage = async ({
+    params,
+}: CreatorPageProps) => {
+    const externalUser = await currentUser();
+    const user = await getUserByUsername(params.username);
+
+    if(!user || user.externalUserId !== externalUser?.id || !user.stream){
+        throw new Error("Unauthorized")
+    }
+
     return (
-        <div>
-            Creator Page
+        <div className="h-full">
+            <StreamPlayer 
+                user={user}
+                stream={user.stream}
+            />
         </div>
     )
 }
